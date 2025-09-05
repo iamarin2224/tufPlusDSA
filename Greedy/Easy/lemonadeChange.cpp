8899e33b2a7ac3bbb3e7ed2c18cbb70e7d9ab17a
Arin Das <iamarindas@gmail.com>
2025-09-05 13:40:04 +0530

    Lemonade Change

---
+++ Greedy/Easy/lemonadeChange.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool lemonadeChange(vector<int>& bills){
+    int five = 0, ten = 0;
+
+    for (int i=0; i<bills.size(); i++){
+        if (bills[i] == 5){
+            five++;
+        }
+
+        else if (bills[i] == 10){
+            if (!five) return false;
+
+            five--, ten++;
+        }
+
+        else if (bills[i] == 20){
+            if (!five) return false;
+
+            if (!ten){
+                if (five < 3) return false;
+                five -= 3;
+            }
+            else{
+                ten--, five--;
+            }
+        }
+    }
+
+    return true;
+}
