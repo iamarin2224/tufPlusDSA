5fe323310c454403069a33903e8668a2a13e2517
Arin Das <iamarindas@gmail.com>
2025-09-05 12:41:35 +0530

    Assign Cookies

--- Greedy/Easy/assignCookies.cpp
+++ Greedy/Easy/assignCookies.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findMaximumCookieStudents(vector<int>& Student, vector<int>& Cookie){
-    //your code goes here
+int findMaximumCookieStudents(vector<int>& student, vector<int>& cookie){
+    sort(student.begin(), student.end());
+    sort(cookie.begin(), cookie.end());
+
+    int stuPtr = 0, cookiePtr = 0;
+
+    while (stuPtr<student.size() && cookiePtr<cookie.size()){
+        if (cookie[cookiePtr] >= student[stuPtr]){
+            cookiePtr++;
+            stuPtr++;
+        }
+        else{
+            cookiePtr++;
+        }
+    }
+
+    return stuPtr;
 }
