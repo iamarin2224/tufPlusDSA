6d669c3af638f9461a0b0822bd47b509e4aa0dfd
Arin Das <iamarindas@gmail.com>
2025-09-06 23:36:55 +0530

    Make folder for new section of greedy and its first problem

---
+++ Greedy/Scheduling/shortestJobFirst.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long solve(vector<int>& bt) {
+    if (bt.size() == 1) return 0L;
+    sort(bt.begin(), bt.end());
+
+    int totalWait = 0;
+    int sum = bt[0];
+
+    for(int i = 1; i<bt.size(); i++){
+        totalWait += sum;
+        sum += bt[i];
+    }
+
+    return (long long)(totalWait/bt.size());
+}
