0f7502c0242d2eaaf0899b55ff310f7ee60911e1
Arin Das <iamarindas@gmail.com>
2025-09-13 00:03:28 +0530

    Optimal solution for max consecutive ones

--- Sliding Window/Long:Short_est window/longestOnes.cpp
+++ Sliding Window/Long:Short_est window/longestOnes.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//? Better Soln, TC: O(2*N)
 int longestOnes(vector<int>& nums, int k) {
     int n = nums.size();
 
@@ -21,6 +22,34 @@ int longestOnes(vector<int>& nums, int k) {
         }
         
         maxLen = max(maxLen, r-l+1);
+        r++;
+    }
+    return maxLen;
+}
+
+//? Optimal Soln, TC: O(N)
+int longestOnes(vector<int>& nums, int k) {
+    int n = nums.size();
+
+    int l = 0, r = 0;
+    int maxLen = 0;
+    int zeroes = k;
+
+    while (r < n){
+        if (nums[r] == 0){
+            zeroes++;
+        }
+
+        if (zeroes > k){
+            if (nums[l] == 0){
+                zeroes--;
+            }
+            l--;
+        }
+
+        if (zeroes <= k){
+            maxLen = max(maxLen, r-l+1);
+        }
         r++;
     }
     return maxLen;
