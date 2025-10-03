be9f98af7eb72179f760384e82286cb3de83afc3
Arin Das <iamarindas@gmail.com>
2025-10-03 18:36:21 +0530

    Complete Longest repeating characters with k replacement problem

---
+++ Sliding Window/Long:Short_est window/longRepCharReplace.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int characterReplacement(string s, int k) {
+    int n = s.size();
+
+    int l=0, r=0;
+    int maxFr=0;
+    int maxlen=0;
+    unordered_map<char, int> mpp;
+
+    while (r < n){
+        mpp[s[r]]++;
+        maxFr = max(maxFr, mpp[s[r]]);
+
+        int flips = (r-l+1)-maxFr;
+
+        if (flips > k){
+            mpp[s[l]]--;
+            l++;
+        }
+
+        if (flips <= k){
+            maxlen = max(maxlen, r-l+1);
+        }
+
+        r++;
+    }
+
+    return maxlen;
+}
