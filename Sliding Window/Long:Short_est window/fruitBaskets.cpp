8e8120825ee5ee0732549ad06142783925d351d6
Arin Das <iamarindas@gmail.com>
2025-10-03 17:38:29 +0530

    Code for more efficient and optimal solution

--- Sliding Window/Long:Short_est window/fruitBaskets.cpp
+++ Sliding Window/Long:Short_est window/fruitBaskets.cpp
@@ -54,3 +54,65 @@ int totalFruits(vector<int>& fruits){
 }
 
 //? Same TC, but better code solution
+int totalFruits(vector<int>& fruits){
+    int n = fruits.size();
+
+    if (n<=2) return n;
+
+    int l=0, r=0;
+    int maxLen = 0;
+    unordered_map<int, int> mpp;
+
+    while (r<n){
+        mpp[fruits[r]]++;
+
+        if (mpp.size() > 2){
+            while (mpp.size() > 2){
+                mpp[fruits[l]]--;
+                if (mpp[fruits[l]] == 0){
+                    mpp.erase(fruits[l]);
+                }
+                l++;
+            }
+        }
+
+        if (mpp.size() <= 2){
+            maxLen = max(maxLen, r-l+1);
+        }
+
+        r++;
+    }
+
+    return maxLen;
+}
+
+//? Optimal
+int totalFruits(vector<int>& fruits){
+    int n = fruits.size();
+
+    if (n<=2) return n;
+
+    int l=0, r=0;
+    int maxLen = 0;
+    unordered_map<int, int> mpp;
+
+    while (r<n){
+        mpp[fruits[r]]++;
+
+        if (mpp.size() > 2){
+            mpp[fruits[l]]--;           //Just removing the outer while loop here
+            if (mpp[fruits[l]] == 0){
+                mpp.erase(fruits[l]);
+            }
+            l++;
+        }
+
+        if (mpp.size() <= 2){
+            maxLen = max(maxLen, r-l+1);
+        }
+
+        r++;
+    }
+
+    return maxLen;
+}
