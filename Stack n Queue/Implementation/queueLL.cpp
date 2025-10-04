259e4f99dd4df8906547d842c7e4d046a8bdc8bf
Arin Das <iamarindas@gmail.com>
2025-10-04 17:59:28 +0530

    Stack and Queue using linked list

---
+++ Stack n Queue/Implementation/queueLL.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedListQueue {
+    Node* front;
+    Node* back;
+    int size;
+public:
+    LinkedListQueue() {
+        front = NULL;
+        back = NULL;
+        size = 0;
+    }
+    
+    void push(int x) {
+        Node* temp = new Node(x);
+        if (size == 0){
+            front = temp;
+            back = temp;
+        }
+        else{
+            back->next = temp;
+            back = back->next;
+        }
+        size++;
+    }
+    
+    int pop() {
+        if (size == 0){
+            cout<<"UNDERFLOW!";
+            return -1;
+        }
+        Node* temp = front;
+        int popped = temp->data;
+        if (size == 1){
+            front = NULL;
+            back = NULL;
+        }
+        else{
+            front = front->next;
+        }
+        size--;
+        delete temp;
+
+        return popped;
+    }
+    
+    int peek() {
+        if (size == 0){
+            cout<<"EMPTY!";
+            return -1;
+        }
+        return front->data;
+    }
+    
+    bool isEmpty() {
+        return (size == 0);
+    }
+};
---
+++ Stack n Queue/Implementation/stackLL.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedListStack {
+    Node* head;
+    int size;
+public:
+    LinkedListStack() {
+        head = NULL;
+        size = 0;
+    }
+    
+    void push(int x) {
+        Node* temp = new Node(x);
+        temp->next = head;
+        head = temp;
+        size++;
+    }
+    
+    int pop() {
+        if (size == 0){
+            cout<<"UNDERFLOW!";
+            return -1;
+        }
+        Node* temp = head;
+        int popped = temp->data;
+
+        head = head->next;
+        size--;
+
+        delete temp;
+        return popped;
+    }
+    
+    int top() {
+        if (size == 0){
+            cout<<"Empty!";
+            return -1;
+        }
+        return head->data;
+    }
+    
+    bool isEmpty() {
+        return (size == 0);
+    }
+};
