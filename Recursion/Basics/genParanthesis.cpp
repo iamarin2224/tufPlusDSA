296a207f4895955596784c30007356a91e0e2d5a
Arin Das <iamarindas@gmail.com>
2026-01-17 13:12:47 +0530

    Update logic to pass string by reference

--- Recursion/Basics/genParanthesis.cpp
+++ Recursion/Basics/genParanthesis.cpp
@@ -1,24 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-void func(int open, int close, string s, vector<string> &ans, int n){
+void func(int open, int close, string &s, vector<string> &ans, int n){
     if (open > n) return;
     if (open == close && open + close == 2*n){
         ans.push_back(s);
         return;
     }
 
-    func(open+1, close, s + "(", ans, n);
+    s.push_back('(');
+    func(open+1, close, s, ans, n);
+    s.pop_back();
+
     if (open > close){
-        func(open, close+1, s + ")", ans, n);
+        s.push_back(')');
+        func(open, close+1, s, ans, n);
+        s.pop_back();
     }
 }
 
 vector<string> generateParenthesis(int n) {
-
     vector<string> ans;
-    func(0, 0, "", ans, n);
-
+    string s = "";
+    func(0, 0, s, ans, n);
     return ans;
-     
 }
