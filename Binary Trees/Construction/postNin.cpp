a2c41d553b7c9289fd3916cfbeb4795537d752f8
Arin Das <iamarindas@gmail.com>
2026-02-12 20:16:33 +0530

    Complete all construction problems

---
+++ Binary Trees/Construction/postNin.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+};
+
+TreeNode* recurBuild(vector<int>& postorder, int postStart, int postEnd, vector<int>& inorder, int inStart, int inEnd, unordered_map<int, int>& mpp){
+    if (postStart > postEnd || inStart > inEnd){
+        return nullptr;
+    }
+
+    TreeNode* root = new TreeNode(postorder[postEnd]);
+
+    int inRoot = mpp[root->data];
+    int numsLeft = inRoot-inStart;
+
+    root->left = recurBuild(postorder, postStart, postStart+numsLeft-1, inorder, inStart, inRoot-1, mpp);
+    root->right = recurBuild(postorder, postStart+numsLeft, postEnd-1, inorder, inRoot+1, inEnd, mpp);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    unordered_map<int, int> mpp;
+
+    for (int i=0; i<inorder.size(); i++){
+        mpp[inorder[i]] = i; 
+    }
+
+    TreeNode* root = recurBuild(postorder, 0, postorder.size()-1, inorder, 0, inorder.size()-1, mpp);
+
+    return root;
+}
---
+++ Binary Trees/Construction/preNin.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : data(val) , left(nullptr) , right(nullptr) {}
+};
+
+TreeNode* recurBuild(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, unordered_map<int, int>& mpp){
+    if (preStart > preEnd || inStart > inEnd){
+        return nullptr;
+    }
+
+    TreeNode* root = new TreeNode(preorder[preStart]);
+
+    int inRoot = mpp[root->data];
+    int numsLeft = inRoot-inStart;
+
+    root->left = recurBuild(preorder, preStart+1, preStart+numsLeft, inorder, inStart, inRoot-1, mpp);
+    root->right = recurBuild(preorder, preStart+numsLeft+1, preEnd, inorder, inRoot+1, inEnd, mpp);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    unordered_map<int, int> mpp;
+
+    for (int i=0; i<inorder.size(); i++){
+        mpp[inorder[i]] = i; 
+    }
+
+    TreeNode* root = recurBuild(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, mpp);
+
+    return root;
+}
