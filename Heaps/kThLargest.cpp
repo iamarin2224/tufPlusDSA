94f7c873a8e3648113161cdc1b12d78536fad1f1
Arin Das <iamarindas@gmail.com>
2026-02-25 23:28:03 +0530

    Include the optimal solution

--- Heaps/kThLargest.cpp
+++ Heaps/kThLargest.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//Better Approach
+
 void heapifyDown(vector<int> &nums, int ind, int n){
     int leftInd = 2*ind+1;
     int rightInd = 2*ind+2;
@@ -37,4 +39,48 @@ int kthLargestElement(vector<int>& nums, int k) {
     }
 
     return nums[0];
+}
+
+// Optimal Approach
+
+int randomIndex(int left, int right){
+    srand(time(NULL));
+    return (rand()%(right-left+1))+left;
+}
+
+int partition(vector<int>& nums, int pivotInd, int left, int right){
+    swap(nums[pivotInd], nums[left]);
+
+    int ind = left+1;
+
+    for(int i=left+1; i<=right; i++){
+        if (nums[i] > nums[left]){
+            swap(nums[i], nums[ind]);
+            ind++;
+        }
+    }
+
+    swap(nums[left], nums[ind-1]);
+
+    return ind-1;
+}
+
+int kthLargestElement(vector<int>& nums, int k) {
+    if (k > nums.size()) return -1;
+
+    int left = 0, right = nums.size()-1;
+
+    while (true){
+        int pivotInd = randomIndex(left, right);
+
+        pivotInd = partition(nums, pivotInd, left, right);
+
+        if (pivotInd == k-1) return nums[pivotInd];
+
+        else if (pivotInd > k-1) right = pivotInd-1;
+
+        else left = pivotInd+1;
+    }
+    
+    return -1;
 }
