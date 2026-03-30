52defb29b987c85bb339639b6a07cfc48897e6ca
Arin Das <iamarindas@gmail.com>
2026-03-30 23:16:28 +0530

    Correct floyd warshall negative cycle detection

--- Graphs/Shortest Path Algo/floydWarshall.cpp
+++ Graphs/Shortest Path Algo/floydWarshall.cpp
@@ -20,10 +20,8 @@ void shortestDistance(vector<vector<int>>&matrix) {
     }
 
     for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            if (matrix[i][j] < 0) {
-                //Negative Cycle
-            }
+        if (matrix[i][i] < 0) {
+            //Negative Cycle
         }
     }
 }
